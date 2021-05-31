96613b99359b2df7ac8cf53a3f9cbc505b509183
Navya Divakar <navyasreyas22@gmail.com>
2021-05-31 16:59:22 +0530

    first commit

---
+++ review project/app.js
@@ -0,0 +1,31 @@
+const reviews = [
+    {
+
+id: 1,
+name: "harry",
+job: "UI Designer",
+img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+text:,
+},
+
+id:2,
+name:"tom",
+job:"digital marketer",
+img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+text:,
+},
+
+id:3,
+name:"susan",
+job:" java developer",
+img:,
+text:,
+},
+
+id:4,
+name:"Clara",
+job:"Mean Stack Developer",
+img:,
+text:,
+},
+}];
